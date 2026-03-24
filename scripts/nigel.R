c2d49848d708a66769dd9a82e7db25e65bf396e8
Ven Popov <vencislav.popov@gmail.com>
2026-03-24 09:23:51 +0100

    fix important typo!!!!

--- scripts/nigel.R
+++ scripts/nigel.R
@@ -1,28 +1,28 @@
-current_hour <- function(){
-  #get current time from machine
+current_hour <- function() {
+  # get current time from machine
   current_time <- Sys.time()
 
-  #extract hour from full time string
+  # extract hour from full time string
   hour_string <- format(current_time, "%H")
 
-  #convert to number and return
+  # convert to number and return
   as.numeric(hour_string)
 }
 
-current_hour() #helper function
+current_hour() # helper function
 
-very_fancy_greet <- function(name, hour = current_hour()){
-  if(hour >= 5 & hour <= 11){
-    paste("Good morning", name, sep = " ")
-  } else if(hour == 12){
+very_fancy_greet <- function(name, hour = current_hour()) {
+  if (hour >= 5 & hour <= 11) {
+    paste("Guuuud morning", name, sep = " ")
+  } else if (hour == 12) {
     paste("Bon appétit!", name, sep = " ")
-  } else if(hour >= 13 & hour <= 17){
+  } else if (hour >= 13 & hour <= 17) {
     paste("Good afternoon", name, sep = " ")
-  } else if(hour >= 18 & hour <= 21){
+  } else if (hour >= 18 & hour <= 21) {
     paste("Good evening", name, sep = " ")
-  } else if(hour == 22){
+  } else if (hour == 22) {
     paste("Good night", name, sep = " ")
-  } else{
+  } else {
     paste("Go to bed,", name, sep = " ")
   }
 }
